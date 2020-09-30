0885d7520cbc9653efd6119e75cf1d57c4dd6328
AhmedAbo6 <ahmed.abo.sita66@gmail.com>
2020-09-30 08:35:42 +0200

    added s3

--- djecommerce/settings.py
+++ djecommerce/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'crispy_forms',
     'django_countries',
+    'storages',
 
     'core'
 ]
@@ -150,3 +151,14 @@ STRIPE_PUBLIC_KEY = "config('STRIPE_LIVE_PUBLIC_KEY')"
 STRIPE_SECRET_KEY = "config('STRIPE_LIVE_SECRET_KEY')"
 
 django_heroku.settings(locals())
+
+# AWS
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
